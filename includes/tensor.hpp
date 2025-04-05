324ad311afa3361131a2d43a91ac907286ddd659
sasha_oiya <kam02072006@gmail.com>
2025-04-05 03:32:51 +0300

    Code review.Add tensor's unit tests

--- includes/tensor.hpp
+++ includes/tensor.hpp
@@ -3,6 +3,7 @@
 #include <cassert>
 #include <cstddef>
 #include <iostream>
+#include <stdexcept>
 #include <vector>
 
 #include "matrix.hpp"
@@ -26,44 +27,75 @@ class Tensor final {
           width_(width),
           data_(batch_ * channels_, Matrix<T>(height_, width_)) {}
 
+    template <typename Iter>
+        requires std::input_iterator<Iter>
+    Tensor(size_type batch, size_type channels, Iter begin, Iter end)
+        : batch_(batch), channels_(channels), data_(begin, end) {
+        if (data_.size() != size()) {
+            throw std::invalid_argument("Mismatch between iterators size and new tensor size");
+        }
+        height_ = data_[0].n_rows();
+        width_ = data_[0].n_cols();
+
+        const size_type matrix_size = data_[0].size();
+        for (auto& it : data_) {
+            if (it.size() != matrix_size) {
+                throw std::invalid_argument("Invalid initializer list matrix size");
+            }
+        }
+    }
+
+    Tensor(size_type batch, size_type channels, std::initializer_list<value_type> l)
+        : Tensor(batch, channels, l.begin(), l.end()) {}
+
     Tensor(const Tensor&) = default;
     Tensor& operator=(const Tensor&) = default;
 
     Tensor(Tensor&& rhs) noexcept
-        : std::vector<value_type>(std::move(rhs)),
-          batch_(std::exchange(rhs.batch_, 0)),
+        : batch_(std::exchange(rhs.batch_, 0)),
           channels_(std::exchange(rhs.channels_, 0)),
           height_(std::exchange(rhs.height_, 0)),
-          width_(std::exchange(rhs.width_, 0)) {}
+          width_(std::exchange(rhs.width_, 0)),
+          data_ (std::move (rhs.data_)){}
 
     Tensor& operator=(Tensor&& rhs) noexcept {
-        std::vector<value_type>::operator=(std::move(rhs));
+        data_ = std::move (rhs.data_);
         batch_ = std::exchange(rhs.batch_, 0);
         channels_ = std::exchange(rhs.channels_, 0);
         height_ = std::exchange(rhs.height_, 0);
         width_ = std::exchange(rhs.width_, 0);
+
+        return *this;
     }
 
     ~Tensor() = default;
 
     //--------------------------------
     constexpr value_type& operator[](size_type n, size_type c) {
-        assert(i < batch_ && j < channels_);
+        if (batch_ <= n || channels_ <= c) {
+            throw std::invalid_argument("Tensor access fail");
+        }
         return data_[n * channels_ + c];
     }
 
     constexpr value_type& operator[](size_type n, size_type c) const {
-        assert(i < batch_ && j < channels_);
+        if (batch_ <= n || channels_ <= c) {
+            throw std::invalid_argument("Tensor access fail");
+        }
         return data_[n * channels_ + c];
     }
 
     constexpr T& operator[](size_type n, size_type c, size_type h, size_type w) {
-        assert(i < batch_ && j < channels_);
+        if (batch_ <= n || channels_ <= c || height_ <= h || width_ <= w) {
+            throw std::invalid_argument("Tensor access fail");
+        }
         return data_[n * channels_ + c][h, w];
     }
 
     constexpr T& operator[](size_type n, size_type c, size_type h, size_type w) const {
-        assert(i < batch_ && j < channels_);
+        if (batch_ <= n || channels_ <= c || height_ <= h || width_ <= w) {
+            throw std::invalid_argument("Tensor access fail");
+        }
         return data_[n * channels_ + c][h, w];
     }
 
@@ -72,7 +104,8 @@ class Tensor final {
     int height() const { return height_; }
     int width() const { return width_; }
 
-    size_type size() const noexcept { return batch_ * channels_ * height_ * width_; }
+    size_type capacity() const noexcept { return batch_ * channels_ * height_ * width_; }
+    size_type size() const noexcept { return batch_ * channels_; }
     constexpr auto& data() noexcept { return data_; }
     constexpr auto& data() const noexcept { return data_; }
 
--- tests/unit_test/tensor.cpp
+++ tests/unit_test/tensor.cpp
@@ -1,20 +1,92 @@
-#include <gtest/gtest.h>
-
 #include "tensor.hpp"
+
+#include <gtest/gtest.h>
 
 TEST(tensor, ctor) {
     Tensor<double> tensor = {2, 2, 2, 2};
 
     EXPECT_EQ(tensor.batch(), 2);
     EXPECT_EQ(tensor.channels(), 2);
-    EXPECT_EQ((tensor[0,0].size()), 4);
+    EXPECT_EQ((tensor[0, 0].size()), 4);
     EXPECT_EQ((tensor[0, 1].size()), 4);
-    EXPECT_EQ((tensor[1,1].size()), 4);
+    EXPECT_EQ((tensor[1, 1].size()), 4);
 }
 
 TEST(tensor, ctor_init) {
     Tensor<double> tensor = {2, 2, 2, 2};
     tensor[1, 1, 1, 0] = 4;
 
-    EXPECT_EQ((tensor[1,1,1,0]), 4);
+    EXPECT_EQ((tensor[1, 1, 1, 0]), 4);
+}
+
+TEST(tensor, ctor_init_list) {
+    Tensor<double> tensor = { 1, 2, {Matrix<double>(2,3), Matrix<double>(2,3)} };
+    tensor[0, 1, 1, 0] = 4;
+
+    EXPECT_EQ((tensor[0, 1, 1, 0]), 4);
+}
+
+TEST(tensor, ctor_init_list_matrix) {
+    Tensor<double> tensor = { 1, 2, {Matrix<double>(1, 2, {5, 5}), Matrix<double>(1, 2, {6, 7})} };
+    EXPECT_EQ((tensor[0, 1, 0, 1]), 7);
+}
+
+TEST(tensor, copy_ctor) {
+    Tensor<double> tensor = { 1, 2, {Matrix<double>(2,1), Matrix<double>(2,1)} };
+    Tensor<double> lhs = tensor;
+
+    EXPECT_TRUE(tensor.capacity() == lhs.capacity());
+    const auto batch_ = lhs.batch(), channels_ = lhs.channels(), height_ = lhs.height(), width_ = lhs.width();
+    for (size_t n = 0; n < batch_; ++n)
+        for (auto c = 0; c < channels_; ++c)
+            for (auto h = 0; h < height_; ++h)
+                for (auto w = 0; w < width_; ++w)
+                    EXPECT_EQ((tensor[n, c, h, w]), (lhs[n, c, h, w]));
+}
+
+TEST(tensor, copy_assignment) {
+    Tensor<double> tensor = { 1, 2, {Matrix<double>(2,1), Matrix<double>(2,1)} };
+    Tensor<double> lhs = {0, 0};
+    lhs = tensor;
+
+    EXPECT_TRUE(tensor.capacity() == lhs.capacity());
+    const auto batch_ = lhs.batch(), channels_ = lhs.channels(), height_ = lhs.height(), width_ = lhs.width();
+    for (size_t n = 0; n < batch_; ++n)
+        for (auto c = 0; c < channels_; ++c)
+            for (auto h = 0; h < height_; ++h)
+                for (auto w = 0; w < width_; ++w)
+                    EXPECT_EQ((tensor[n, c, h, w]), (lhs[n, c, h, w]));
+}
+
+TEST(tensor, move_ctor) {
+    Tensor<double> tensor = { 1, 1, {Matrix<double>(2,1)} };
+
+    std::vector<std::vector<double>> data = {};
+    const int cols_ = tensor.height(), rows_ = tensor.width();
+    for (size_t i = 0; i < cols_; ++i) {
+        std::vector<double> row = {};
+        for (auto j = 0; j < rows_; ++j) row.push_back(tensor[0, 0, i, j]);
+        data.push_back(row);
+    }
+    Tensor<double> lhs = std::move(tensor);
+
+    for (size_t i = 0; i < cols_; ++i)
+        for (auto j = 0; j < rows_; ++j) EXPECT_EQ(data[i][j], (lhs[0, 0, i, j]));
+}
+
+TEST(tensor, move_assignment) {
+    Tensor<double> tensor = { 1, 1, {Matrix<double>(2,1)} };
+
+    std::vector<std::vector<double>> data = {};
+    const int cols_ = tensor.height(), rows_ = tensor.width();
+    for (size_t i = 0; i < cols_; ++i) {
+        std::vector<double> row = {};
+        for (auto j = 0; j < rows_; ++j) row.push_back(tensor[0, 0, i, j]);
+        data.push_back(row);
+    }
+    Tensor<double> lhs = {};
+    lhs = std::move(tensor);
+
+    for (size_t i = 0; i < cols_; ++i)
+        for (auto j = 0; j < rows_; ++j) EXPECT_EQ(data[i][j], (lhs[0, 0, i, j]));
 }
